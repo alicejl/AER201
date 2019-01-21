6416daa48237f2bcda948f59ba3878d3b01c11bb
alicejl <46697092+alicejl@users.noreply.github.com>
2019-01-21 17:24:59 -0500

    Updated main.c with more details in run

--- main.c
+++ main.c
@@ -25,18 +25,39 @@ void main(void){
  */
 
 void initRun(void){
-	enable interupts for peripherals
-	set up peripherals
-	enable interupt for timer
-	set timer to 3:50 minutes
-	start timer
-	start motors
+	enable interupts for peripherals;
+	set up peripherals;
+	initiate stack;
+	enable interupt for timer;
+	set timer to 3:50 minutes;
+	start timer;
+	start motors;
 }
 
 /**
  * @brief regular run of robot
  */
 
-void run(void){
-	
+void run(void){ //should there and back be the same function?
+	int atCanister = 0; //is there a canister detected: 0 no, 1 yes
+	int deployBall = 0; //should a ball be deployed: 0 no, 1 yes
+	int atEnd = 0; // reused variable, 1 at turn around, and 1 for termination
+	float dist=0; //var for distance
+
+	while(1){
+		atEnd = isEnd();
+		if (atEnd ==1){
+			atEnd=0;
+			turnAround(); //initate turn
+			break;
+		atCanister = isCanister();
+		if (atCanister == 1){
+			runMotor(0); //pause motor
+			deployBall = canisterStatus(&dist); //check conditions: is a ball inside is it past 20cm
+			if (deployBall == 1){
+				dropBall();
+			}
+			push(flag); //info for return
+			enque(dist, deployBall, 'L')//add to queue for run stats
+	}
 }
