a16e5de20ae8ebd6d5e71ef02c7bbd4d8845d770
alicejl <46697092+alicejl@users.noreply.github.com>
2019-03-01 16:22:47 -0500

    commented and updated stack

--- stack.c
+++ stack.c
@@ -13,11 +13,11 @@
 
 struct stack{
     int pointer;//current stack pointer
-    int array[14];//max 10 canisters has extra to prevent overflow
+    int array[12];//max 10 canisters has extra to prevent overflow
 };
 
 struct log{
-    float dist;
+    int dist;
     int serviced;
     int full;//1 for full 0 for empty
 };
@@ -25,28 +25,28 @@ struct log{
 struct queue{
     int addP;//adding pointer
     int popP;//removing pointer
-    struct log array[14];//array
+    struct log array[12];//array
 };
 
 //initiate stacks and queues
 struct stack s;
 struct queue q;
-struct stack rtnS;
+struct stack rtnS;//canister locations in the queue for adjusting during returns
 
-void initStacks(void){
+void initStacks(void){//initializes the starting pointers for each stack
     s.pointer=-1;
     q.addP=0;
     q.popP=0;
     rtnS.pointer=-1;
 }
 
-void push(int info){
-    s.pointer=s.pointer+1;
-    if ((s.pointer<14)&&(s.pointer>=0)){
-        s.array[s.pointer]=info;   
+void push(int info,int open){
+    s.pointer=s.pointer+1;//increment pointer
+    if ((s.pointer<12)&&(s.pointer>=0)){
+        s.array[s.pointer]=info;//add info to array 
     }
-    if (info==1){//check on rtn open to left
-        rtnS.pointer = rtnS.pointer + 1;
+    if (open==1){//check on rtn open to left
+        rtnS.pointer = rtnS.pointer + 1;//add the q location to rtnS
         rtnS.array[rtnS.pointer] = q.addP;
     }
 }
@@ -54,39 +54,43 @@ void push(int info){
 int pop(void){
     int data;
     if (s.pointer>=0){
-        data=s.array[s.pointer];
-        s.array[s.pointer] = 0;
-        s.pointer = s.pointer-1;
+        data=s.array[s.pointer];//extract data
+        s.array[s.pointer] = 0;//reset the array value
+        s.pointer = s.pointer-1;//decrement pointer
     }
     return data;
 }
 
-void enqueue(float dist, int ball, int full){
-    q.array[q.addP].dist = dist;
+void enqueue(int dist, int ball, int full){
+    q.array[q.addP].dist = dist;//add data
     q.array[q.addP].serviced = ball;
     q.array[q.addP].full = full;
-    q.addP = q.addP + 1;
+    q.addP = q.addP + 1;//increment pointer
 }
 
-void dequeue(float* dist, int* ball, int* full){
+void dequeue(int* dist, int* ball, int* full){
     if (q.popP<q.addP){
-        *dist = q.array[q.popP].dist;
-        q.array[q.popP].dist = 0;
+        *dist = q.array[q.popP].dist;//return data via pointers
+        q.array[q.popP].dist = 0;//reset array value
         *ball = q.array[q.popP].serviced;
         q.array[q.popP].serviced = 0;
         *full = q.array[q.popP].full;
         q.array[q.popP].full = 0;
-        q.popP = q.popP + 1;
+        q.popP = q.popP + 1;//increment pop pointer
     }
 }
 
 void adjQ(int ball, int full){//adjusts q for ball status on return journey
     int index;
     if (rtnS.pointer>=0){
-        index=rtnS.array[rtnS.pointer];
-        rtnS.array[rtnS.pointer] = 0;
-        rtnS.pointer = rtnS.pointer-1;
+        index=rtnS.array[rtnS.pointer];//extract index of q location to be adjusted
+        rtnS.array[rtnS.pointer] = 0;//reset array value
+        rtnS.pointer = rtnS.pointer-1;//decrement pointer
     }
-    q.array[index].serviced = ball;
+    q.array[index].serviced = ball;//adjust the q values
     q.array[index].full = full;
+}
+
+int qLen(void){//returns the length of the queue
+    return q.addP;
 }
--- stack.h
+++ stack.h
@@ -23,17 +23,23 @@ struct queue;
 void initStacks(void);
 
 /**
- * @brief push info onto s, pushes the end of q pointer onto rtn if info is 2 and can. opens to the left
+ * @brief push info onto s, pushes the end of q pointer onto rtn if open is 0 and can. opens to the left
  */
-void push(int info);
+void push(int info,int open);
 
 /**
  * @brief pops data off stack and decrements pointer
  */
 int pop(void);
 
-void enqueue(float dist, int ball, int full);
-void dequeue(float* dist, int* ball, int* full);
+/** @brief enqueues data*/
+void enqueue(int dist, int ball, int full);
 
+/** @brief dequeues data*/
+void dequeue(int* dist, int* ball, int* full);
 
+/** @brief returns the length of the queue*/
+int qLen(void);//returns addP
+
+/**adjusts the ball and full values for the return trip*/
 void adjQ(int ball, int full);
