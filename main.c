828ed2add016ba8320275c6ad14afc38233806e8
alicejl <46697092+alicejl@users.noreply.github.com>
2019-09-26 22:50:03 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 /**
  * @file
+ * @author Michael Ding
+ * @author Tyler Gamvrelis
  * @author Alice Liang
  * 
  * Created on January 18, 2019, 4:07 PM
@@ -30,17 +32,14 @@ void main(void){
 	// RD2 is the character LCD RS
 	// RD3 is the character LCD enable (E)
 	// RD4-RD7 are character LCD data lines
-    	
-            
-    	// Set all A/D ports to digital (pg. 222)
-    	
-        
-        initBoard();
+    // Set all A/D ports to digital (pg. 222)
+    
+    initBoard();
    
-    	initLCD();// Initialize LCD
-        lcd_set_ddram_addr(LCD_LINE1_ADDR);
-        printf("    Welcome!");
-        __delay_ms(1000);
+    initLCD();// Initialize LCD
+    lcd_set_ddram_addr(LCD_LINE1_ADDR);
+    printf("    Welcome!");
+    __delay_ms(1000);
 	while(1){
         initMenu();
         menuLoop();
@@ -50,55 +49,44 @@ void main(void){
 void menuLoop(void){
 	// Main loop
 	while(1){
-        	// RB1 is the interrupt pin, so if there is no key pressed, RB1 will be
-	        // 0. The PIC will wait and do nothing until a key press is signaled
-        	while(PORTBbits.RB1 == 0){  continue;   }
+        // RB1 is the interrupt pin, so if there is no key pressed, RB1 will be
+	    // 0. The PIC will wait and do nothing until a key press is signaled
+        while(PORTBbits.RB1 == 0){  continue;   }
         
-        	// Read the 4-bit character code
-	        unsigned char keypress = (PORTB & 0xF0) >> 4;
+        // Read the 4-bit character code
+	    unsigned char keypress = (PORTB & 0xF0) >> 4;
         
-        	// Wait until the key has been released
-	        while(PORTBbits.RB1 == 1){  continue;   }
+        // Wait until the key has been released
+	    while(PORTBbits.RB1 == 1){  continue;   }
         
-        	Nop(); // Apply breakpoint here to prevent compiler optimizations
+        Nop(); // Apply breakpoint here to prevent compiler optimizations
         
-        	unsigned char temp = keys[keypress];
-	        if (temp == '1'){ 
-                runDisp();
-                run();
-                return;
-            }
+        unsigned char temp = keys[keypress];
+	    if (temp == '1'){ 
+            runDisp();
+            run();
+            return;
+        }
 		
-            else if (temp == '2'){
-                showLog();
-                return;
-            }
+        else if (temp == '2'){
+            showLog();
+            return;
+        }
             
-            else if (temp == '3'){
-                dateTime();
-                return;
-            }
-            
-            else if (temp == '4'){
-                lcd_clear();
-                LATAbits.LATA3 = 0;//active low reset
-                __delay_ms(10);
-                LATAbits.LATA3 = 1;
-                lcd_set_ddram_addr(LCD_LINE1_ADDR);
-                printf("  Calibrating!");
-                lcd_set_ddram_addr(LCD_LINE2_ADDR);
-                printf("  ~ 15 seconds");
-                __delay_ms(15000);
-            }
-            else{
-                lcd_clear();
-                lcd_set_ddram_addr(LCD_LINE1_ADDR);
-                printf(" Invalid Input!");
-                lcd_set_ddram_addr(LCD_LINE2_ADDR);
-                printf("Select 1,2 or 3");
-                __delay_us(1000000);
-                return;
-            }
+        else if (temp == '3'){
+            dateTime();
+            return;
+        }
+        
+        else{
+            lcd_clear();
+            lcd_set_ddram_addr(LCD_LINE1_ADDR);
+            printf(" Invalid Input!");
+            lcd_set_ddram_addr(LCD_LINE2_ADDR);
+            printf("Select 1,2 or 3");
+            __delay_us(1000000);
+            return;
+        }
     }
 }
 
@@ -115,8 +103,7 @@ void initBoard(void){
     ADCON1 = 0b00001111;
     LATE=0x00;
     TRISE=0x00;
-    LATEbits.LATE0 = 1;//sensor arduino reset
-    LATAbits.LATA3 = 1;//drive arduino reset
+    LATEbits.LATE0 = 1;
     
     return;
 }
